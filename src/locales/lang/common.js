9419cc8ea05f73e7aedf1e404d7fb7d2b29b7c45
fcanbolat <fcanbolat@gmail.com>
2024-05-14 11:02:37 +0300

    .

--- src/locales/lang/common.js
+++ src/locales/lang/common.js
@@ -1612,7 +1612,7 @@ const common = {
       6: "Su",
     },
     copyright: "{title}. Cryptocurrency Platform",
-    built_by: "Built using {title} platform by {title}",
+    built_by: "Built using LOM platform by LOM",
     password_upper: "At least one uppercase letter",
     password_lower: "At least one lowercase letter",
     password_number: "At least one number",
